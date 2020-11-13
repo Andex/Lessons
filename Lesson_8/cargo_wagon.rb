db2e70602315b57439a850521c77af3ebd4a078c
Andex <anyanov1998@gmail.com>
2020-11-13 22:20:19 +0300

    Добавление атрибута объема и операции с ним в грузовом вагоне

--- Lesson_8/cargo_wagon.rb
+++ Lesson_8/cargo_wagon.rb
@@ -1,5 +1,26 @@
 class CargoWagon < Wagon
-  def initialize
+  attr_reader :general_volume
+  attr_accessor :number_of_occupied_volume
+
+  def initialize(general_volume)
+    @general_volume = general_volume
+    @number_of_occupied_volume = 0
     super(:cargo)
+  end
+
+  def take_volume(volume)
+    return nil if number_of_occupied_volume >= general_volume || volume > remaining_volume
+    self.number_of_occupied_volume += volume
+  end
+
+  def remaining_volume
+    general_volume - number_of_occupied_volume
+  end
+
+  protected
+
+  def validate!
+    super()
+    raise "Invalid value general volume" unless general_volume > 0 && (general_volume.is_a?(Integer) || general_volume.is_a?(Float))
   end
 end
