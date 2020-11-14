803e786ab4ccbfdc5f5592027aba408e3fe46683
Andex <anyanov1998@gmail.com>
2020-11-14 18:00:46 +0300

    Добавление атрибута объема и операции с ним в грузовом вагоне

--- Lesson_8/cargo_wagon.rb
+++ Lesson_8/cargo_wagon.rb
@@ -9,7 +9,7 @@ class CargoWagon < Wagon
   end
 
   def take_volume(volume)
-    return nil if number_of_occupied_volume >= general_volume || volume > remaining_volume
+    raise "Invalid value for adding volume" if number_of_occupied_volume >= general_volume || volume > remaining_volume
     self.number_of_occupied_volume += volume
   end
 
