ead7d4ee4228329d9d94ab1800576b6f11571dc9
Andex <anyanov1998@gmail.com>
2020-11-27 21:13:22 +0300

    Добавление модуля Accessors

--- Lesson_10/modules.rb
+++ Lesson_10/modules.rb
@@ -24,3 +24,31 @@ module InstanceCounter
     end
   end
 end
+
+module Accessors
+  def attr_accessor_with_history(*attrs)
+    attrs.each do |name_attr|
+      var_name = "@#{name_attr}".to_sym
+      var_history = "@#{name_attr}_history"
+      define_method(name_attr) { instance_variable_get(var_name) }
+
+      define_method("#{name_attr}=".to_sym) do |value|
+        instance_variable_set(var_history, []) unless instance_variable_get(var_history)
+        instance_variable_get(var_history) << value
+        instance_variable_set(var_name, value)
+      end
+
+      define_method("#{name_attr}_history".to_sym) { instance_variable_get(var_history) }
+    end
+  end
+
+  def strong_attr_accessor(name_attr, class_attr)
+    var_name = "@#{name_attr}".to_sym
+    define_method(name_attr) { instance_variable_get(var_name) }
+    define_method("#{name_attr}=".to_sym) do |value|
+      raise TypeError, 'Несоответсвие типа переменной' if value.class != class_attr
+
+      instance_variable_set(var_name, value)
+    end
+  end
+end
