931ae6f92f92abcb9f7bd59fac2d58db72bb3525
Andex <anyanov1998@gmail.com>
2020-11-28 23:58:43 +0300

    Добавление модуля Validation

--- Lesson_10/modules.rb
+++ Lesson_10/modules.rb
@@ -46,9 +46,57 @@ module Accessors
     var_name = "@#{name_attr}".to_sym
     define_method(name_attr) { instance_variable_get(var_name) }
     define_method("#{name_attr}=".to_sym) do |value|
-      raise TypeError, 'Несоответсвие типа переменной' if value.class != class_attr
+      raise TypeError, 'Variable type mismatch' if value.class != class_attr
 
       instance_variable_set(var_name, value)
+    end
+  end
+end
+
+module Validation
+  def self.included(base)
+    base.include(InstanceMethods)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    attr_accessor :validates
+
+    def validate(attr_name, *args)
+      @validates ||= []
+      @validates << [attr_name, args]
+    end
+  end
+
+  module InstanceMethods
+    def validate!
+      self.class.validates.each do |validation|
+        send("validate_#{validation[1][0]}", validation[0], validation[1])
+      end
+    end
+
+    def valid?
+      validate!
+      true
+    rescue RuntimeError
+      false
+    end
+
+    protected
+
+    def validate_presence(attr_name, _args)
+      value = instance_variable_get("@#{attr_name}")
+      raise ArgumentError, 'Empty string or nil' if value.nil? || value == ''
+    end
+
+    def validate_format(attr_name, args)
+      raise ArgumentError, 'Regex not passed' if args[1].nil?
+      raise ArgumentError, 'Regex mismatch' unless instance_variable_get("@#{attr_name}") =~ args[1]
+    end
+
+    def validate_type(attr_name, args)
+      raise ArgumentError, 'Class name not passed' if args[1].nil?
+      raise TypeError, 'Variable type mismatch' unless value.instance_of?(args[1]) || value.instance_of?(args[-1])
     end
   end
 end
